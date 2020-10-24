82d1aac4445ba1e958ad612beef2d6643b8068f4
Felipe Marinho <felipmarinho@gmail.com>
2020-10-24 11:33:36 -0300

    test: ensure SignUp prevent AddAccount to be called multiple times

--- src/presentation/pages/signup/signup.spec.tsx
+++ src/presentation/pages/signup/signup.spec.tsx
@@ -148,4 +148,11 @@ describe('SignUp', () => {
       passwordConfirmation: password
     })
   })
+
+  test('Should call Authentication only once', async () => {
+    const { sut, addAccountSpy } = makeSut()
+    await simulateValidSubmit(sut)
+    await simulateValidSubmit(sut)
+    expect(addAccountSpy.callsCount).toBe(1)
+  })
 })
