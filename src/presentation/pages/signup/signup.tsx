f99816a562e30469315d24a6a65875e6a22971f3
Felipe Marinho <felipmarinho@gmail.com>
2020-10-24 11:01:30 -0300

    feat: ensure SignUp enable submit button if for is valid

--- src/presentation/pages/signup/signup.tsx
+++ src/presentation/pages/signup/signup.tsx
@@ -53,7 +53,7 @@ const SignUp: React.FC<Props> = ({ validation }: Props) => {
             data-testid="submit"
             className={Styles.submit}
             type="submit"
-            disabled
+            disabled={!!state.nameError || !!state.emailError || !!state.passwordError || !!state.passwordConfirmationError }
           >
             Entrar
           </button>
