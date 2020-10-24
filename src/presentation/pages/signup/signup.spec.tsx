4466a1f870d15885e365cfb249f1fc6c4fff6f3a
Felipe Marinho <felipmarinho@gmail.com>
2020-10-24 10:56:02 -0300

    test: ensure SignUp change passwordConfirmation status to correct if validation succeeds

--- src/presentation/pages/signup/signup.spec.tsx
+++ src/presentation/pages/signup/signup.spec.tsx
@@ -92,4 +92,10 @@ describe('SignUp', () => {
     Helper.populateField(sut, 'password')
     Helper.testStatusForField(sut, 'password')
   })
+
+  test('Should show valid passwordConfirmation state if Validation succeeds', () => {
+    const { sut } = makeSut()
+    Helper.populateField(sut, 'passwordConfirmation')
+    Helper.testStatusForField(sut, 'passwordConfirmation')
+  })
 })
