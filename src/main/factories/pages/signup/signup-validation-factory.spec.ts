58fb37133cdfd389fe491d4d470c1ba6a55e07fd
Felipe Marinho <felipmarinho@gmail.com>
2020-10-24 20:24:43 -0300

    feat: add signup composition

--- src/main/factories/pages/signup/signup-validation-factory.spec.ts
+++ src/main/factories/pages/signup/signup-validation-factory.spec.ts
@@ -6,10 +6,10 @@ describe('SignUpValidationFactory', () => {
   test('Should make ValidationCompose with correct validations', () => {
     const composite = makeSignUpValidation()
     expect(composite).toEqual(ValidationComposite.build([
-      ...Builder.field('name').required().min(5).email().build(),
+      ...Builder.field('name').required().min(5).build(),
       ...Builder.field('email').required().email().build(),
       ...Builder.field('password').required().min(5).build(),
-      ...Builder.field('passwordConfirmation').required().min(5).build()
+      ...Builder.field('passwordConfirmation').required().sameAs('password').build()
     ]))
   })
 })
