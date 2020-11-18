8f7583db1210657d96a88a880b4aab3f11be6c14
Felipe Marinho <felipmarinho@gmail.com>
2020-11-18 06:30:55 -0300

    refactor: move duplicated code to helper methods

---
+++ src/main/test/cypress/integration/login-mocks.ts
@@ -0,0 +1,11 @@
+import * as Helper from '../support/http-mocks'
+import faker from 'faker'
+
+export const mockInvalidCredendialsError = (): void => Helper.mockInvalidCredendialsError(/login/)
+export const mockUnexpectedError = (): void => Helper.mockUnexpectedError(/login/, 'POST')
+export const mockOk = (): void => Helper.mockOk(/login/, 'POST', {
+  accessToken: faker.random.uuid()
+})
+export const mockInvalid = (): void => Helper.mockOk(/login/, 'POST', {
+  invalidProperty: faker.random.uuid()
+})
