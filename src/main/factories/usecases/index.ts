c9136e9c6675a06bebd2bb39fd7fea693aeee34c
Felipe Marinho <felipmarinho@gmail.com>
2021-02-23 06:32:22 -0300

    feat: add index for exports

---
+++ src/main/factories/usecases/index.ts
@@ -0,0 +1,3 @@
+export * from './add-account/remote-add-account-factory'
+export * from './authetication/remote-authentication-factory'
+export * from './load-survey-list/remote-load-survey-list-factory'
