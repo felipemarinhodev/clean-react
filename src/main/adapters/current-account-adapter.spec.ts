224db74b7b169809848a4537f1aafa1c5c815def
Felipe Marinho <felipmarinho@gmail.com>
2021-03-23 06:22:34 -0300

    refactor: remove unused code

--- src/main/adapters/current-account-adapter.spec.ts
+++ src/main/adapters/current-account-adapter.spec.ts
@@ -1,7 +1,6 @@
-import { LocalStorageAdapter } from '@/infra/cache/local-storage-adapter'
 import { mockAccountModel } from '@/domain/test'
-import { setCurrentAccountAdapter, getCurrentAccountAdapter } from './current-account-adapter'
-import { UnexpectedError } from '@/domain/errors'
+import { LocalStorageAdapter } from '@/infra/cache/local-storage-adapter'
+import { getCurrentAccountAdapter, setCurrentAccountAdapter } from './current-account-adapter'
 
 jest.mock('@/infra/cache/local-storage-adapter')
 
@@ -11,12 +10,6 @@ describe('CurrentAccounAdapter', () => {
     const setSpy = jest.spyOn(LocalStorageAdapter.prototype, 'set')
     setCurrentAccountAdapter(account)
     expect(setSpy).toHaveBeenCalledWith('account', account)
-  })
-
-  test('Should throw UnexpectedError', () => {
-    expect(() => {
-      setCurrentAccountAdapter(undefined)
-    }).toThrow(new UnexpectedError())
   })
 
   test('Should call LocalStorageAdapter.get with correct value', () => {
